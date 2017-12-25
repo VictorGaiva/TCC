b90ea7cef920f7a930d3b09ecce49e8a391173f1
Victor Gaíva <victorgaiva2014@gmail.com>
2017-12-25 17:37:06 -0300

    Created a file

---
+++ classifier.py
@@ -0,0 +1,17 @@
+"""
+Where everything happends
+"""
+import keras
+
+def train(input_path, output_file):
+    """
+    Uses the batches in the input folder to train the NN
+    """
+
+def main():
+    """
+    Main
+    """
+
+if __name__ == '__main__':
+    main()
