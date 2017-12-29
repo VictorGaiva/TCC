45bcdd585a9124aac64711a83f41efd3e109fd3c
Victor Gaíva <victorgaiva2014@gmail.com>
2017-12-28 22:59:14 -0300

    Added an option to plot some data analisys about the batches

--- statistics_extraction.py
+++ statistics_extraction.py
@@ -4,6 +4,13 @@ import os
 import numpy as np
 import feature_extraction as fe
 
+#plotting
+import pandas as pd
+from scipy import stats, integrate
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
 def get_audio_metadata(vinhetas_path, propagandas_path, talks_path):
     """
     Gets the statiscts about the audio files
@@ -58,13 +65,46 @@ def get_features_metadata(features_path):
         print('\tSTD:', val.std())
         print("\n")
 
+def get_batches_distribution(features_path, batch_name):
+    """
+    Plots a distribution of the values of each atribute in the batch files
+    """
+    feat = fe.open_atr_file(features_path)
+
+    feat = np.transpose(feat)
+
+    sns.set(color_codes=True)
+    maxxx = 0
+    i = 0
+    for val in feat:
+        i += 1
+        print("Ploting", i)
+        myplot = sns.distplot(val, hist=False)
+        if max(val) > maxxx:
+            maxxx = max(val)
+
+        #setting axes
+    myplot.set_xlim(left=0, right=maxxx)
+    myplot.set_ylim(bottom=0, top=15)
+
+        #making figure
+    fig = myplot.get_figure()
+    #plotting
+    fig.savefig(batch_name + ".png")
+    #cleaning
+    fig.clf()
+
+
 def main():
     """Main foo"""
     vinhetas_path = '../capturas/por classe/vinhetas/'
     propagandas_path = '../capturas/por classe/propagandas/'
     talks_path = '../capturas/por classe/talks/'
     #get_audio_metadata(vinhetas_path, propagandas_path, talks_path)
-    get_features_metadata("./output")
+    #get_features_metadata("./output")
+    get_batches_distribution("../dataset/batches/test_x.atr05", "test4")
+    get_batches_distribution("../dataset/batches/validate_x.atr05", "validate4")
+    get_batches_distribution("../dataset/batches/train_x.atr05", "train4")
 
 if __name__ == '__main__':
     main()
